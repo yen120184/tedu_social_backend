ddd070ec3b6b0e477d6bf8642dfe01afc362df8d
Yen <yennsqn@gmail.com>
2022-02-22 17:07:20 +0700

    Create application structure

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,22 @@
-import express from 'express';
+// import express from 'express';
 
-const port = process.env.PORT || 5000;
+// const port = process.env.PORT || 5000;
 
-const app = express();
+// const app = express();
 
-app.get('/',(req, res) => {
-    res.send('API is running...');
-});
+// app.get('/',(req, res) => {
+//     res.send('API is running...');
+// });
 
-app.listen(port, () =>{
-    console.log(`Server is listening on port ${port}`);
-})
+// app.listen(port, () =>{
+//     console.log(`Server is listening on port ${port}`);
+// })
+
+import App from "./app";
+import { IndexRoute } from "./modules/index";
+
+const routes=[new IndexRoute()];
+
+const app=new App(routes);
+
+app.listen();
